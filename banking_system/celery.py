16f3fa6e275039a92935fa1c1e90eaba6fdc4ad0
Met-Hack <53992146+Met-Hack@users.noreply.github.com>
2021-10-12 12:47:52 +0530

    Update celery.py

--- banking_system/celery.py
+++ banking_system/celery.py
@@ -3,7 +3,12 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking_system.settings')
+
+app = Celery('banking_system')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
